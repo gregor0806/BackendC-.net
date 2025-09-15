e2f19ca4c4718e0a0e18883398e059e6b6565654
Gregor Maska <mas240749@spengergasse.at>
2025-09-15 19:34:16 +0200

    gregor 1

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,7 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Services registrieren
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+
+// 2. App bauen
 var app = builder.Build();
 
-// Route definieren: GET /api/todo
-app.MapGet("/api/todo", () => "Hello World");
+// 3. Middleware aktivieren
+app.UseCors("AllowAll");
 
+// 4. Endpunkte definieren
+app.MapGet("/api/todo", () => "Hello World");
+app.MapControllers();
+
+// 5. App starten
 app.Run();
--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed82aba496bdec1e3dd824a1a5081e0d7ad91af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ff9e3a3d56379334dd267302fffa973248201de")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
